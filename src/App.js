a8314f84c71b7bc1f456d4096dd1add363d17b3e
Saurabh Gupta <saurabh.gupta@inmorphis.com>
2023-02-07 22:24:43 +0530

    Definitions and Theme Toggler--Second commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 import Header from './components/Header/Header';
+import Definitions from './components/Definitions/Definitions';
+import Switch from '@mui/material/Switch';
+import { FormControlLabel } from '@mui/material';
 
 function App() {
+
+  //STATES
+
   const [word, setWord] = useState('');
   const [meanings, setMeanings] = useState([]);
-
+  const [category, setCategory] = useState('en');
+  const [lightMode, setLightMode] = useState(false)
+  
+  //FETCH DATA 
   const dictionaryApi = async () => {
     try {
-      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/plane');
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`);
 
       setMeanings(data.data)
     } catch (error){
@@ -19,16 +28,52 @@ function App() {
     }
   }
 
-  console.log(meanings);
-
   useEffect(() => {
     dictionaryApi();
-  }, [])
+  }, [word,category])
 
   return (
-    <div className="App" style={{height: '100vh', backgroundColor: '#282c34', color: 'white'}}>
-      <Container maxWidth='md' style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
-        <Header />
+    <div 
+            className="App" 
+            style={{height: '100vh', 
+            backgroundColor: lightMode ? "#DEE4E7" : '#282c34', 
+            color: lightMode ? "black" : 'white', 
+            transition: 'all 0.4s linear'
+      }}>
+
+      <Container 
+            maxWidth='md' 
+            style={{display: 'flex', 
+                    flexDirection: 'column', 
+                    height: '100vh', 
+                    justifyContent: 'space-evenly'
+                    }}>
+        <div 
+            style={{position: "absolute", 
+                  top: 0,
+                  right: 15, 
+                  paddingTop: 10
+                  }}>
+          
+          {/* THEME SWITCHER TOGGLE */}
+          <FormControlLabel
+            value="Light"
+            control={<Switch color="primary" />}
+            label={lightMode ? "Dark Mode" : "Light Mode"}
+            labelPlacement="Light"
+            checked={lightMode} onChange={() => setLightMode(!lightMode)}
+          />
+        </div>
+        <Header 
+            category={category}
+            setCategory = {setCategory}
+            word={word} setWord={setWord} 
+            lightMode={lightMode} 
+          />
+        {meanings && (<Definitions word={word}
+                                   meanings={meanings} 
+                                   category={category}
+                                   lightMode={lightMode} />)}
       </Container>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,48 @@
 import { TextField, ThemeProvider, createTheme, MenuItem} from '@mui/material';
 import React from 'react';
 import './Header.css'
+import categories from '../../data/category'
 
-const Header = () => {
+const Header = ({category, setCategory, word, setWord, lightMode}) => {
 
     const darkTheme = createTheme({
         palette: {
-          mode: 'dark',
+          mode: lightMode ? "light" : "dark",
         },
       });
 
+    const handleChange = (language) => {
+        setCategory(language);
+        setWord("");
+    }
+
     return (
         <div className='header'>
-            <span className='title' style={{color: 'white'}}>Dictionary</span>
+            <span className='title' style={{color: lightMode ? "black" :  'white'}}>{word ? word : "Dictionary"}</span>
             <div className='inputs'>
                 <ThemeProvider theme={darkTheme}>
-                     <TextField id="standard-basic" label="Enter a word" variant="standard" />
+                        <TextField 
+                            className="search"
+                            id="standard-basic" 
+                            label="Enter a word" 
+                            variant="standard" 
+                            value={word}
+                            onChange={(e) => setWord(e.target.value)}
+                        />
                         <TextField
+                            className="select"
                             select
-                            label="Select"
-                            helperText="Please select your language"
+                            label="Language"
+                            value={category}
+                            onChange={(e) => handleChange(e.target.value)}
                         >
-                            <MenuItem>
-                                English
-                            </MenuItem>
+                            {
+                                categories.map((lang) => (
+                                    <MenuItem key={lang.label} value={lang.label}>
+                                    {lang.value}
+                                </MenuItem>
+                                ))
+                            }
                         </TextField>
                 </ThemeProvider>
             </div>
